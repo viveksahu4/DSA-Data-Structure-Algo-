ced26f559d2eb50d04eddb3681d064baf633d0cd
Vivek sahu <vickysv1610@gmail.com>
2026-02-28 21:21:19 +0530

    changes

--- 2.Linked List/SinglyLinkedList/InsertAtspecificPos.cpp
+++ 2.Linked List/SinglyLinkedList/InsertAtspecificPos.cpp
@@ -1,63 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Node{
+class node{
     public:
     int data;
-    Node* next = NULL;
-
-    Node(int data){
+    node* next;
+    
+    node(int data){
         this->data = data;
         this->next = NULL;
-
     }
-
-
 };
 
-void InsertAtSpePos(Node* &head, Node* &tail, int value,int pos){
-    
 
+void insertatspec(node* &head,node* &tail,int value,int pos){
     if(pos == 1 || head == NULL){
-        Node* temp = new Node(value);
+        node* temp = new node(value);
         temp->next = head;
         head = temp;
         tail = temp;
+
+        
+        
     }
     else{
-        Node* temp = new Node(value);
+        node* temp = new node(value);
         int i = 1;
-        Node* tempq = head;
-        while(i < pos - 1  && tempq != NULL ){
-            tempq = tempq -> next;
+        node* nhead = head;
+        while(i < pos - 1  && nhead != NULL){
+            nhead = nhead->next;
             i++;
-            
         }
-        
-        temp->next = tempq->next;
-        tempq->next = temp;
+        temp->next = nhead->next;
+        nhead->next  = temp;
     }
 }
 
-
-
-void printll(Node* head){
+void printll(node* head){
     while(head != NULL){
         cout<<head->data<<" ";
         head = head->next;
     }
-
 }
 int main(){
-    Node* head = NULL;
-    Node* tail = NULL;
-
-
-        InsertAtSpePos(head,tail,10,1);
-        InsertAtSpePos(head,tail,500,2);
-        InsertAtSpePos(head,tail,100,3);
-
+    node* head = NULL;
+    node* tail = NULL;
+    
+    int n;
+    cin>>n;
+    while(n--){
+        int value,pos;
+        cin>>value>>pos;
+        insertatspec(head,tail,value,pos);
+    }
+    
     printll(head);
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -101,39 +109,26 @@ int main(){
 // };
 
 // void InsertAtSpePos(Node* &head, Node* &tail, int value,int pos){
-//     Node* newtemp   = new Node(value);
+    
 
-//     //case1: Empty List
-//     if(head == NULL){
-//         head =  newtemp;
-//         tail  = newtemp;
-//         return;
-//     }
-
-//     //case2: Insert at head
-//     if(pos == 1){
-//         newtemp->next =  head;
-//         head = newtemp;
-//         return;
-//     }
-
-//     //traverse to (pos - 1) node
-//     Node* temp = head;
-//     int i = 1;
-//     while(i < pos -1 && temp->next != NULL){
-//         temp = temp->next;
-//         i++;
-//     }
-
-//     //Insert at end
-//     if(temp->next == NULL){
-//         temp->next = newtemp;
-//         tail = newtemp;
-
+//     if(pos == 1 || head == NULL){
+//         Node* temp = new Node(value);
+//         temp->next = head;
+//         head = temp;
+//         tail = temp;
 //     }
 //     else{
-//         newtemp->next = temp->next;
-//         temp->next = newtemp;
+//         Node* temp = new Node(value);
+//         int i = 1;
+//         Node* tempq = head;
+//         while(i < pos - 1  && tempq != NULL ){
+//             tempq = tempq -> next;
+//             i++;
+            
+//         }
+        
+//         temp->next = tempq->next;
+//         tempq->next = temp;
 //     }
 // }
 
@@ -149,13 +144,112 @@ int main(){
 // int main(){
 //     Node* head = NULL;
 //     Node* tail = NULL;
-//     int n;
-//     cin>>n;
-//     while(n--){
-//         int a,b;
-//         cin>>a>>b;
-//         InsertAtSpePos(head,tail,a,b);
-//     }
+
+
+//         InsertAtSpePos(head,tail,10,1);
+//         InsertAtSpePos(head,tail,500,2);
+//         InsertAtSpePos(head,tail,100,3);
 
 //     printll(head);
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // #include<bits/stdc++.h>
+// // using namespace std;
+// // class Node{
+// //     public:
+// //     int data;
+// //     Node* next = NULL;
+
+// //     Node(int data){
+// //         this->data = data;
+// //         this->next = NULL;
+
+// //     }
+
+
+// // };
+
+// // void InsertAtSpePos(Node* &head, Node* &tail, int value,int pos){
+// //     Node* newtemp   = new Node(value);
+
+// //     //case1: Empty List
+// //     if(head == NULL){
+// //         head =  newtemp;
+// //         tail  = newtemp;
+// //         return;
+// //     }
+
+// //     //case2: Insert at head
+// //     if(pos == 1){
+// //         newtemp->next =  head;
+// //         head = newtemp;
+// //         return;
+// //     }
+
+// //     //traverse to (pos - 1) node
+// //     Node* temp = head;
+// //     int i = 1;
+// //     while(i < pos -1 && temp->next != NULL){
+// //         temp = temp->next;
+// //         i++;
+// //     }
+
+// //     //Insert at end
+// //     if(temp->next == NULL){
+// //         temp->next = newtemp;
+// //         tail = newtemp;
+
+// //     }
+// //     else{
+// //         newtemp->next = temp->next;
+// //         temp->next = newtemp;
+// //     }
+// // }
+
+
+
+// // void printll(Node* head){
+// //     while(head != NULL){
+// //         cout<<head->data<<" ";
+// //         head = head->next;
+// //     }
+
+// // }
+// // int main(){
+// //     Node* head = NULL;
+// //     Node* tail = NULL;
+// //     int n;
+// //     cin>>n;
+// //     while(n--){
+// //         int a,b;
+// //         cin>>a>>b;
+// //         InsertAtSpePos(head,tail,a,b);
+// //     }
+
+// //     printll(head);
+// // }
