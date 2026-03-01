b73476ad582fd73ab48a3a7289762484f04c78b5
Vivek sahu <vickysv1610@gmail.com>
2026-03-01 22:22:31 +0530

    Sort

---
+++ LoveBabar/Arrays/sort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortarr(int num[],int size){
+    int left = 0;
+    int right = size - 1;
+
+    while(left < right){
+        if(num[left] == 0){
+            left++;
+        }
+        else if(num[right] == 1){
+            right--;
+        }
+        else{
+            swap(num[left],num[right]);
+        }
+    }
+    
+}
+
+
+void print(int num[],int size){
+    for(int i=0;i<size;i++){
+        cout<<num[i]<<" ";
+    }
+
+}
+
+int main(){
+    int arr[6] = {0,1,0,1,1,0};
+
+    sortarr(arr,6);
+    print(arr,6);
+    return 0;
+}
