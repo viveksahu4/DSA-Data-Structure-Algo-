c828cc7a8444f21299dae57426c80e88555ada9a
Vivek sahu <vickysv1610@gmail.com>
2026-03-02 21:22:21 +0530

    Solid Rectangle Pattern

---
+++ LoveBabar/Patterns/1soliderectangle.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int n = 3;
+    // int m = 5;
+
+    // for(int i=0;i<n;i++){
+    //     for(int j = 0;j<m;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    int p;
+    int q;
+    cin>>p>>q;
+
+    for(int i=0;i<p;i++){
+        for(int j = 0;j<q;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
