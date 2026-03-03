54d9ae72f3568fbe55588df260b34de0fac05539
Vivek sahu <vickysv1610@gmail.com>
2026-03-04 00:16:11 +0530

    Star + (plus)

---
+++ LoveBabar/Patterns/10StarPlus.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+
+
+
+int main(){
+    int n = 5;
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<=n;j++){
+            if(i == 3|| j == 3){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+
+        }
+        cout<<endl;
+    }
+}
