e4c47dc9d1833cd112406dab2a7b63c4afdacfdb
Vivek sahu <vickysv1610@gmail.com>
2026-03-03 23:45:56 +0530

    Type casting

---
+++ LoveBabar/Patterns/Typecasting.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+// int a = 65;
+// char ch = (char)a;
+// cout<<ch;
+
+
+int n = 4;
+for(int i=1;i<=n;i++){
+    for(int j = 65;j< 65 + n;j++){
+        char ch = (char)j;
+        cout<<ch<<" ";
+    }
+    cout<<endl;
+
+}
+
+}
+
+
+
