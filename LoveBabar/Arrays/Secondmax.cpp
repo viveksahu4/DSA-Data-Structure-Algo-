8fdf752eef860082c48c3e89d1e1931426f63f1c
Vivek sahu <vickysv1610@gmail.com>
2026-03-07 23:34:36 +0530

    changes for negative condition -4 -2 -3 -11 -5

--- LoveBabar/Arrays/Secondmax.cpp
+++ LoveBabar/Arrays/Secondmax.cpp
@@ -5,9 +5,13 @@ int getMax(int num[],int n){
     int max = INT_MIN;
     int max2 = INT_MIN;
     for(int i=0;i<n;i++){
-        if(num[i] > max){
+        if(num[i] > max  ){
             max2 = max;
             max = num[i];
+        }
+        if(num[i] < max && num[i] > max2){
+            max2 = num[i];
+            
         }
     }
 
