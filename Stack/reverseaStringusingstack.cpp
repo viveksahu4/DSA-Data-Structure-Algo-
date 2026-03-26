c4828b26b3d4d0f34a74f8f5df343c32e568a996
Vivek sahu <vickysv1610@gmail.com>
2026-03-26 17:36:54 +0530

    reverse string with the help of stack

--- Stack/reverseaStringusingstack.cpp
+++ Stack/reverseaStringusingstack.cpp
@@ -4,23 +4,35 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-// class stringreverse{
-//     public:
+class stringreverse{
+    public:
 
 
-// };
+};
 
 int main(){
+    string str = "vivek";
 
     stack<char> s;
-    s.push('L');
-    s.push('o');
-    s.push('v');
-    s.push('e');
+    // s.push('L');
+    // s.push('o');
+    // s.push('v');
+    // s.push('e');
+    for(int i=0;i<str.length();i++){
+        char ch = str[i];
+        s.push(ch);
+
+    }
+    string ans = "";
 
     while(!s.empty()){
-    cout<<s.top()<<endl;
+    char ch  = s.top();
+    ans.push_back(ch);
+
     s.pop();
     }
+
+
+    cout<<ans<<endl;
     return 0;
 }
