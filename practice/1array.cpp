42352b3f420bcc751f36b04d0c13a8c6e975d4cd
Vivek sahu <vickysv1610@gmail.com>
2026-04-05 10:54:27 +0530

    practice

--- practice/1array.cpp
+++ practice/1array.cpp
@@ -1,27 +1,72 @@
-// 2. duplicate check
+//3.Insert element at end 
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int> arr = {12,34,12,89};
-    bool check = true;
-    for(int i=0;i<arr.size();i++){
-        for(int j = arr.size() - 1;j>i;j--){
-            if(arr[j] == arr[i]){
-                check  = false;
-                break;
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter element of the array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
 
-            }
-        }
     }
-    if(check){
-        cout<<"No duplicate";
+
+    cout<<"Without Insert O/P: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+
     }
-    else{
-        cout<<"duplicate";
+    cout<<endl;
+
+
+    int value;
+    cout<<"Enter element you want to insert: ";
+    cin>>value;
+    arr[n] = value;
+    n++;
+
+    cout<<endl;
+    cout<<"Final o/p with Insert: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+
     }
+
 
     return 0;
+
+
+    
 }
+
+
+
+
+// // 2. duplicate check
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     vector<int> arr = {12,34,12,89};
+//     bool check = true;
+//     for(int i=0;i<arr.size();i++){
+//         for(int j = arr.size() - 1;j>i;j--){
+//             if(arr[j] == arr[i]){
+//                 check  = false;
+//                 break;
+
+//             }
+//         }
+//     }
+//     if(check){
+//         cout<<"No duplicate";
+//     }
+//     else{
+//         cout<<"duplicate";
+//     }
+
+//     return 0;
+// }
 
 
 
